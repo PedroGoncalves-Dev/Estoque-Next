95b5d6a650ab65a527ccf7d377950535b6c4dc1a
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-10-22 12:17:45 -0300

    DropDown adicionado para ações dos produtos

--- src/app/(dashBoard)/produtos/__components/Table-Coluns.tsx
+++ src/app/(dashBoard)/produtos/__components/Table-Coluns.tsx
@@ -3,6 +3,22 @@
 import { Badge } from "@/components/ui/badge";
 import { Produto } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import {
+  CircleIcon,
+  ClipboardCopyIcon,
+  DeleteIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -33,7 +49,56 @@ export const columns: ColumnDef<Produto>[] = [
     cell: (row) => {
       const produto = row.row.original;
       const label = getStatusLabel(produto.status);
-      return <Badge>{label}</Badge>;
+      return (
+        <Badge
+          variant={label === "Em estoque" ? "default" : "outline"}
+          className="gap-1.5"
+        >
+          <CircleIcon
+            size={14}
+            className={`${label === "Em estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"} `}
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="outline-none">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+
+            <DropdownMenuItem className="cursor-pointer gap-1.5">
+              <EditIcon size={16} />
+              Editar
+            </DropdownMenuItem>
+
+            <DropdownMenuItem className="cursor-pointer gap-1.5">
+              <DeleteIcon size={16} />
+              Deletar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
--- src/data-access/produto/getProduto.ts
+++ src/data-access/produto/getProduto.ts
@@ -7,3 +7,5 @@ export const getProduto = async (): Promise<Produto[]> => {
   console.log("fetching of products..");
   return await db.produto.findMany({});
 };
+
+
